592d1858143bf82241fb5363869cdd04d3db23e8
GeorgeM <G.McClain@bluemtninc.com>
2017-01-12 14:14:55 -0500

    Comments

--- Blue Mountain Connect/Credentials.cs
+++ Blue Mountain Connect/Credentials.cs
@@ -8,9 +8,10 @@ namespace Blue_Mountain_Connect
 {
     public class Credentials
     {
-
+        //Salt
         public string salt = "abc1@3$5^7*9)-+xyz";
 
+        //Generate Random Salt, Not currently being used!
         public String GenSalt(int size)
         {
             var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
@@ -19,6 +20,7 @@ namespace Blue_Mountain_Connect
             return Convert.ToBase64String(buff);
         }
         
+        // Generate Hash
         public String GenHash(String input, String salt)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(input + salt);
@@ -29,6 +31,7 @@ namespace Blue_Mountain_Connect
             return Convert.ToBase64String(hash);
         }
 
+        // Getter and Setter for credentials
         public string Username { get; set; }
         public string Password { get; set; }
     }
